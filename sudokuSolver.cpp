4c384386d0cd536fe59f12fbf023d646f49426f3
kdeleon000@citymail.cuny.edu <kdeleon000@citymail.cuny.edu>
2023-02-22 13:08:48 -0500

    update

--- sudokuSolver.cpp
+++ sudokuSolver.cpp
@@ -44,14 +44,14 @@ class board {
         
         void setBoardStartingValues(){
             this->values = {{5,3,9,0,0,0,4,1,0},
-                            {7,2,8,3,1,4,9,6,5},
+                            {7,0,8,3,0,4,0,0,0},
                             {6,4,1,0,0,0,7,3,0},
-                            {4,6,2,5,3,9,8,7,1},
-                            {3,8,5,7,2,1,6,4,9},
-                            {1,9,7,4,6,8,2,5,3},
-                            {2,5,6,1,8,7,3,9,4},
-                            {9,1,3,0,4,0,5,8,7},
-                            {8,7,4,9,5,3,1,2,6}};
+                            {4,0,2,0,0,0,0,7,0},
+                            {3,8,0,7,0,1,6,4,0},
+                            {1,0,7,4,6,8,2,5,3},
+                            {0,0,0,0,0,0,0,0,0},
+                            {9,0,0,0,4,0,5,8,0},
+                            {8,0,4,0,5,0,0,0,6}};
         }
 
         bool checkIfValueIsValid(vector<int> RowCol, size_t potentialBoardStartingValue){
@@ -124,11 +124,12 @@ class board {
                         this->getPossibleCellValues(cellPossibleValues,cellNumber);
                     }
                     if (cellPossibleValues[cellNumber].empty()){
-                        cellStack.pop();
+                        cellStack.pop(); 
                         cellPossibleValues.erase(cellNumber);
                         curRow = cellStack.top() / this->ROWS;
                         curCol = cellStack.top() % this->ROWS;
                         values[curRow][curCol] = 0;
+                        cellStack.pop();
                     }
                     else { 
                         values[curRow][curCol] = cellPossibleValues[cellNumber].back();
